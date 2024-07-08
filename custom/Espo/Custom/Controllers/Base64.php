cb31c12ebb979ca4e2dc40296830bda8454570f6
abdulkadir <>
2024-07-08 13:39:08 +0300

    add new api Base46

---
+++ custom/Espo/Custom/Controllers/Base64.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Espo\Custom\Controllers;
+
+use Espo\Core\Api\Request;
+use Espo\Core\Api\Response;
+class Base64
+{
+
+    public function postActionBase64(Request $request, Response $response)
+    {
+
+        die('Base64 Controller is working!11111');
+        return true;
+    }
+}
